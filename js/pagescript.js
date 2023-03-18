9c62031bd5d19f75e04bcd344466335b2f2aefa1
luismi4157 <63623916+luismi4157@users.noreply.github.com>
2023-03-17 23:42:11 -0600

    Understanding addEventListener

--- js/pagescript.js
+++ js/pagescript.js
@@ -1,2 +1,12 @@
 "use strict";
 
+let buttons = document.querySelectorAll('nav > button');
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", buttonClick)
+
+    function buttonClick(ev){
+            let name = ev.currentTarget;
+            window.alert(name)
+    }
+};
