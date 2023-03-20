c1215edc74e80bfa7e157ed45b9c71ca988cb62d
luismi4157 <63623916+luismi4157@users.noreply.github.com>
2023-03-19 22:01:12 -0600

    Made buttons functional

--- js/pagescript.js
+++ js/pagescript.js
@@ -1,12 +1,41 @@
 "use strict";
-
+let mainSections = ["home", "gear", "info", "contact"]
 let buttons = document.querySelectorAll('nav > button');
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", buttonClick)
+};
 
-    function buttonClick(ev){
-            let name = ev.currentTarget;
-            window.alert(name)
+function buttonClick(ev){
+    //removes "selected" from every button's class
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].classList.remove("selected");
+    }
+    //adds "selected" to the button that was pressed
+    ev.currentTarget.classList.add('selected');
+    
+    //remove "show" from every section in main
+    for(let section of mainSections){
+        let selectedSection = document.getElementById(`${section}Section`);
+        selectedSection.classList.remove("show");
+        selectedSection.classList.add("hide");
+    }
+    
+    //show selected content section
+    let name = `${ev.currentTarget.name}Section`;
+    let showSection = document.getElementById(name);
+    showSection.classList.remove('hide');
+    showSection.classList.add('show');
+
+    let asideTable = document.getElementById("asideSection")
+    if(name === "homeSection"){
+       asideTable.classList.remove("show");
+       asideTable.classList.remove("hide");
+       asideTable.classList.add("show");
+    }
+    else{
+        asideTable.classList.remove("show");
+       asideTable.classList.remove("hide");
+       asideTable.classList.add("hide");
     }
 };
