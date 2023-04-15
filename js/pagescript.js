caa983ada9a70eaa40deeea86d8fa75966ca9eb5
luismi4157 <luismi4157@gmail.com>
2023-04-14 22:13:31 -0600

    Added theme change functionality

--- js/pagescript.js
+++ js/pagescript.js
@@ -1,10 +1,13 @@
 "use strict";
 let mainSections = ["home", "info", "contact"]
 let buttons = document.querySelectorAll('nav > button');
+let themeButton = document.getElementById("themeButton");
 
 for(let i = 0; i < mainSections.length; i++){
     buttons[i].addEventListener("click", buttonClick)
 };
+
+themeButton.addEventListener("click", function() {changeTheme("css/theme.css")})
 
 function buttonClick(ev){
     //removes "selected" from every button's class
@@ -38,4 +41,23 @@ function buttonClick(ev){
        asideTable.classList.remove("hide");
        asideTable.classList.add("hide");
     }
+};
+
+function changeTheme(href, onoff){
+    var existingNode = 0 //get existing stylesheet node if it already exists:
+  for (var i = 0; i < document.styleSheets.length; i++) {
+    if (document.styleSheets[i].href && document.styleSheets[i].href.indexOf(href) > -1) existingNode = document.styleSheets[i].ownerNode
+  }
+  if (onoff == undefined) onoff = !existingNode //toggle on or off if undefined
+  if (onoff) { //TURN ON:
+    if (existingNode) return onoff //already exists so cancel now
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = href;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  } else { //TURN OFF:
+    if (existingNode) existingNode.parentNode.removeChild(existingNode)
+  }
+  return onoff
 };
